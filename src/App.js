0b6038f7479225935c989a9f2778323f8805764f
MarinaKononenko <macdanieltatenda00@gmail.com>
2024-07-08 17:37:38 +0300

    None finished data init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './components/List/List';
+import store  from "../src/utils/store";
 
 function App() {
+  const [data, setData] = useState(store);
+  console.log(data.map())
   return (
     <div className="App">
       <List/>
