91f9d6f84ef3d54da633eb9d4242e6f0cbd7d096
MarinaKononenko <macdanieltatenda00@gmail.com>
2024-07-08 17:08:25 +0300

    :tada: Input button : card component🎉

---
+++ src/components/input/InputCard.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Paper, InputBase, Button, IconButton } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
+import { makeStyles } from "@mui/styles";
+import { styled } from "@mui/system";
+import { ThemeProvider, createTheme, alpha } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#FF5733",
+        // light: will be calculated from palette.primary.main,
+        // dark: will be calculated fro // contrastText: will be calculated to contrast with palette.primary.main
+      },
+      secondary: {
+        main: "#E0C2FF",
+        light: "#F5EBFF",
+        // dark: will be calculated from palette.secondary.main,
+        contrastText: "#47008F",
+      },
+    },
+  });
+
+const useStyle = makeStyles((theme) => ({
+  card: {
+    textAlign: "start",
+    margin: theme.spacing(0, 1, 1, 1),
+    paddingBottom: theme.spacing(4),
+    paddingLeft: theme.spacing(2)
+  },
+  confirm:{
+    margin: theme.spacing(0, 1, 1, 1),
+    display:"flex",
+    alignItems: "start",
+    
+  },
+  input:{
+    backgroundColor:"#fff"
+  }
+}));
+
+const ButtonWrapper = styled(Button)(({theme}) => ({
+    color:"#fff",
+    background:"green",
+    "&:hover":{
+        background: "#000",
+        color:"#fff"
+    }
+   
+}))
+
+
+export default function InputCard({setOpen}) {
+  const classes = useStyle();
+  return (
+    <div>
+      <div>
+        <Paper className={classes.card}>
+          <InputBase
+            multiline
+            fullwidth
+            onBlur={() => setOpen(false)}
+            inputProps={{
+              classes: classes.input,
+            }}
+            placeholder="Enter a title for this card"
+          />
+        </Paper>
+      </div>
+      <div className={classes.confirm}>
+        <ButtonWrapper onClick={() => setOpen(false)} > Add Cart</ButtonWrapper>
+        <IconButton>
+          <ClearIcon onClick={() => setOpen(false)}/>
+        </IconButton>
+      </div>
+    </div>
+  );
+}
--- src/components/input/InputContainer.js
+++ src/components/input/InputContainer.js
@@ -1,28 +1,28 @@
-import { Typography, Paper, colors, } from "@mui/material";
+import { Typography, Paper, colors } from "@mui/material";
 import React from "react";
-import { makeStyles,} from "@mui/styles";
+import { makeStyles } from "@mui/styles";
 import { ThemeProvider, createTheme, alpha } from "@mui/material/styles";
-import { lime, purple } from '@mui/material/colors';
-import { styled } from '@mui/system';
-
+import { lime, purple } from "@mui/material/colors";
+import { styled } from "@mui/system";
+import InputCard from "./InputCard";
+import { useState } from "react";
+import { Collapse } from '@mui/material';
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#FF5733',
-        // light: will be calculated from palette.primary.main,
-        // dark: will be calculated from palette.primary.main,
-        // contrastText: will be calculated to contrast with palette.primary.main
-      },
-      secondary: {
-        main: '#E0C2FF',
-        light: '#F5EBFF',
-        // dark: will be calculated from palette.secondary.main,
-        contrastText: '#47008F',
-      },
+  palette: {
+    primary: {
+      main: "#FF5733",
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated fro // contrastText: will be calculated to contrast with palette.primary.main
     },
-  });
-  
+    secondary: {
+      main: "#E0C2FF",
+      light: "#F5EBFF",
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: "#47008F",
+    },
+  },
+});
 
 const useStyle = makeStyles((theme) => ({
   root: {
@@ -30,24 +30,27 @@ const useStyle = makeStyles((theme) => ({
   },
 }));
 
-const PaperWrapper = styled(Paper)(({theme}) => ({
-    padding: theme.spacing(1, 1, 1, 2),
-    margin: theme.spacing(0, 1, 1, 1),
-    '&:hover' : {
-        backgroundColor: theme.palette.secondary.main,
-        color: '#fff'
-        
-    }
-}))
+const PaperWrapper = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1, 1, 1, 2),
+  margin: theme.spacing(0, 1, 1, 1),
+  "&:hover": {
+    backgroundColor: theme.palette.secondary.main,
+    color: "#fff",
+  },
+}));
 export default function InputContainer() {
+  const [open, setOpen] = useState(false)
   const classes = useStyle();
   return (
     <div className={classes.root}>
-      <PaperWrapper >
-       <Typography>
-        + Add a card
-       </Typography>
+      <Collapse in={open}>
+      <InputCard setOpen={setOpen} />
+      </Collapse>
+    <Collapse in={!open}>
+    <PaperWrapper elevation={0} onClick={() => setOpen(!open)}>
+        <Typography>+ Add a card</Typography>
       </PaperWrapper>
+    </Collapse>
     </div>
   );
 }
