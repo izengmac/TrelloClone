fb84199d62ef2d0985997cc6d76623dd31cbf07c
MarinaKononenko <macdanieltatenda00@gmail.com>
2024-07-12 11:23:35 +0300

    Unfinished Part 4

--- src/components/nav/SideMenu.js
+++ src/components/nav/SideMenu.js
@@ -1,10 +1,38 @@
 import React, { useState } from "react";
+import { ThemeProvider, createTheme, alpha } from "@mui/material/styles";
 import { makeStyles } from "@mui/styles";
 import { Drawer } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FF5733",
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated fro // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: "#E0C2FF",
+      light: "#F5EBFF",
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: "#47008F",
+    },
+  },
+});
 
 const useStyles = makeStyles({
   drawer: {
     width: "400px",
+  },
+  menu: {
+    marginTop: theme.spacing(2),
+    display: "flex",
+    justifyContent: "space-around",
+  },
+  box: {
+    width: "45%",
+    height: "90px",
+    backgroundColor: "blue",
+    borderRadius:"9px"
   },
 });
 
@@ -19,7 +47,24 @@ function SideMenu({ openSideMenu, setOpenSideMenu }) {
         onClose={() => setOpenSideMenu(false)}
       >
         <div className={classes.drawer}>
-          <h1>Hello World</h1>
+          <div className={classes.menu}>
+            <div 
+            className={classes.box}
+            style= {{
+              backgroundImage:'url(https://thumbs.dreamstime.com/b/spring-summer-landscape-blue-sky-clouds-river-boat-green-trees-narew-countryside-grass-poland-water-leaves-58070004.jpg)',
+              backgroundRepeat:'no-repeat',
+              backgroundSize:'cover'
+            }}
+            ></div>
+            <div 
+            className={classes.box}
+            style= {{
+              backgroundImage:'url(https://digitalsynopsis.com/wp-content/uploads/2017/06/beautiful-color-palettes-combinations-schemes-metro-ui-colors.png)',
+              backgroundRepeat:'no-repeat',
+              backgroundSize:'cover'
+            }}
+            ></div>
+          </div>
         </div>
       </Drawer>
     </div>
